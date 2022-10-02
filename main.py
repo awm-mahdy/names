705f5b212e353580aee5a35eb906af64d70ca5de
awm-mahdy <82960837+awm-mahdy@users.noreply.github.com>
2022-10-02 16:02:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask
+import random
+from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+def main():
+ m = random.choice(['hima','man','amakwkw'])
+ return f"{m}"
+app.run(debug=True, port=os.getenv("PORT", default=5000))
